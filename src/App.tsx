6dfe1977759993acd015cf89b0ea6ac2da8742d5
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-14 15:49:17 -0500

    chore: Add patient form and patients list components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+import PatientForm from './components/PatientForm';
+import PatientsList from './components/PatientsList';
+
 export default function App() {
-	return <div>App</div>;
+	return (
+		<div className='container mx-auto mt-20'>
+			<h1 className='font-black text-5xl text-center md:w-2/3 md:mx-auto'>
+				Seguimiento de pacientes {''}
+				<span className='text-indigo-700'>Veterinaria</span>
+			</h1>
+
+			<div className='mt-12 md:flex	'>
+				<PatientForm />
+				<PatientsList />
+			</div>
+		</div>
+	);
 }
 
