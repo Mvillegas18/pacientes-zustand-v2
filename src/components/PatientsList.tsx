345a36b99d39f1bb5664cbcbb7dd497cd7083f09
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-15 11:15:44 -0500

    chore: Add PatientDetails component for displaying patient details

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,3 +1,39 @@
+import { usePatientStore } from '../store';
+import PatientDetails from './PatientDetails';
+
 export default function PatientsList() {
-	return <div>PatientsList</div>;
+	const { patients } = usePatientStore();
+
+	return (
+		<div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
+			{patients.length ? (
+				<>
+					<h2 className='font-black text-3xl text-center'>
+						Listado de pacientes
+					</h2>
+					<p className='text-xl mt-5 mb-10 text-center'>
+						Administra tus {''}
+						<span className='bg-indigo-600 font-bold'>Pacientes y citas</span>
+					</p>
+
+					{patients.map((patient) => (
+						<PatientDetails
+							key={patient.id}
+							patient={patient}
+						/>
+					))}
+				</>
+			) : (
+				<>
+					<h2 className='font-black text-3xl text-center'>No hay pacientes</h2>
+					<p className='text-xl mt-5 mb-10 text-center'>
+						Comieza agregando pacientes {''}
+						<span className='text-indigo-600 font-bold'>
+							y apareceran en este lugar
+						</span>
+					</p>
+				</>
+			)}
+		</div>
+	);
 }
